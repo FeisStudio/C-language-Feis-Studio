803074fc564be8152de2413b92726426bfd9422c
徹先生 <huangyuanzyf@gmail.com>
2020-04-17 22:04:33 +0800

    加入专案档案。

--- 8.8 - 消費金額計算的練習 (使用 switch 述句)/8.8.c
+++ 8.8 - 消費金額計算的練習 (使用 switch 述句)/8.8.c
@@ -1,14 +1,20 @@
 #pragma warning(disable:4996)
 #include<stdio.h>
-int main()
-{
-	int number, sum = 0;
+int main() {
+	int id, total = 0;
+	printf("Please enter the ID:\n");
+	do {
+		scanf("%d", &id);
+		switch (id) {
+		case 1: total += 90; break;
+		case 2: total += 75; break;
+		case 3: total += 83; break;
+		case 4: total += 89; break;
+		case 5: total += 71; break;
+		default:printf("Error: No corresponding id");
+		}
+	} while (id != 0);
 
-	printf("Please enter the number:");
-	scanf("%d", &number);
-	swith (num != 0) 
-
-	}
-	
+	printf("Total:%d\n", total);
 	return 0;
 }
---
+++ 9 - 計次型的重複執行 (for 述句)/9.c
@@ -0,0 +1,10 @@
+#pragma warning(disable:4996)
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	for (;i <= 10; i++) {
+		printf("%d\n", i);
+	}
+	return 0;
+}
