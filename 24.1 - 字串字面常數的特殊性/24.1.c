df3dd982c6395b02d5f34834f60de428effb0282
UglyCat <huangyuanzyf@gmail.com>
2020-07-12 12:43:30 +0800

    加入專案檔案。

--- 24.1 - 字串字面常數的特殊性/24.1.c
+++ 24.1 - 字串字面常數的特殊性/24.1.c
@@ -5,11 +5,12 @@ int main() {
 	char strA[] = "test";
 	char *strB = "test";	//字串字面常數可隱性轉型成字元指標
 							//只能讀不能寫
+							//一個整體,編譯器自動一個唯讀空間
 
 	strA[0] = 'T';	// (O)
-	strB[0] = 'T';	// (X) 未定義行爲
+	strB[0] = 'T';	// (X) 未定義行爲	只能讀取,不能寫
 
-	strA = "Test";	// (X) 編譯失敗		
+	strA = "Test";	// (X) 編譯失敗		陣列之間是不可以直接賦值的
 	strB = "Test";	// (O) 
 
 
