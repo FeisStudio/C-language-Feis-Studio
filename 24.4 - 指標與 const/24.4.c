908800c3bd94f2d24afc28f3d360f5bfe1890f6e
徹先生 <huangyuanzyf@gmail.com>
2020-07-12 16:07:13 +0800

    加入專案檔案。

--- 24.4 - 指標與 const/24.4.c
+++ 24.4 - 指標與 const/24.4.c
@@ -9,13 +9,13 @@ int main() {
 	const char *strC[] = 'test';
 
 	strA[0] = 'T';	// O					只能改字元不能修改陣列
-	strB[0] = 'T';	// X	（未定義行爲）
+	//strB[0] = 'T';	// X	（未定義行爲）	唯讀
 	strC[0] = 'T';	// X	（編譯失敗）		不能修改字元，但可以把字串指向另外一個字串
-
-	strA = strB;	// (char [])=(char *)			(X)
-	strA = strC;	// (char [])=(const char *)		(X)
-	strB = strA;	// (char *)=(char [])			(O)
-	strB = strC;	// (char *)=(const char *)		(X)
+	 
+	strA = strB;	// (char [])=(char *)			(X)	陣列不能直接被賦值
+	strA = strC;	// (char [])=(const char *)		(X)	陣列不能直接被賦值
+	strB = strA;	// (char *)=(char [])			(O)	字元陣列存到字元指標	字元陣列可以隱性轉成字元指標
+	strB = strC;	// (char *)=(const char *)		(X)	唯讀變量(字元)不能修改
 	strC = strA;	// (const char *)=(char [])		(O)
 	strC = strB;	// (const char *)=(char *)		(O)
 
