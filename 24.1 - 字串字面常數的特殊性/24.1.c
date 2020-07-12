6ad4a9e1080a6308aec8ad56c9f5587263a08ced
徹先生 <huangyuanzyf@gmail.com>
2020-07-12 16:05:09 +0800

    加入專案檔案。

--- 24.1 - 字串字面常數的特殊性/24.1.c
+++ 24.1 - 字串字面常數的特殊性/24.1.c
@@ -5,7 +5,7 @@ int main() {
 	char strA[] = "test";
 	char *strB = "test";	//字串字面常數可隱性轉型成字元指標
 							//只能讀不能寫
-							//一個整體,編譯器自動一個唯讀空間
+							//一個整體,編譯器自動一個唯讀空間 
 
 	strA[0] = 'T';	// (O)
 	strB[0] = 'T';	// (X) 未定義行爲	只能讀取,不能寫
